a071788565d9ed88f48553cc1184ee6658187fd7
Lucas Braida <lucbnaza@gmail.com>
2023-01-05 17:34:28 -0300

    Added personal website link

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
 export default function Footer(props){
     return (
         <footer className="footerContainer">
-            <h4>Made with <span><FavoriteRoundedIcon fontSize="inherit" style={{ color: 'red' }} /></span> by Lucas Braida. <a href='https://github.com/LucasBraida' target="_blank">Come check more cool stuff</a></h4>
+            <h4>Made with <span><FavoriteRoundedIcon fontSize="inherit" style={{ color: 'red' }} /></span> by Lucas Braida. <a href='https://lucasbraida.com' target="_blank">Come check more cool stuff</a></h4>
             <h4>With the beautiful background image <a href='https://www.freepik.com/vectors/watercolor'target="_blank">Watercolor vector created by renata.s - www.freepik.com</a></h4>
         </footer>
     )
